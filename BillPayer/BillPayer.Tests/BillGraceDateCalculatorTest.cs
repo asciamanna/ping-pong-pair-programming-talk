dd2a116fe8e94afe81f5e05830b8d786875c37ac
Anthony Sciamanna <asciamanna@gmail.com>
2017-03-16 16:56:02 -0400

    Step 1. Create test class and start first unit test.

---
+++ BillPayer/BillPayer.Tests/BillGraceDateCalculatorTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace BillPayer.Tests {
+  [TestFixture]
+  public class BillGraceDateCalculatorTest {
+
+    [Test]
+    public void CalculateMortgageDate() {
+      var subject = new BillGraceDateCalculator();
+    }
+  }
+}
